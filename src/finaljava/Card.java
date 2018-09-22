852f649d74dd32a0b6fc6317a0efa630677da9ae
J. García <u0x004a@gmail.com>
2018-09-22 11:59:12 -0600

    Final project

---
+++ src/finaljava/Card.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finaljava;
+
+/**
+ *
+ * @author J. Garcia, jyo.garcia at gmail.com
+ */
+public class Card {
+
+	private int id;
+
+	public static enum type{
+		HARTS,
+		DIAMONDS,
+		SPADES,
+		CLOVERS
+	}
+	public static enum value{
+		V1,
+		V2,
+		V3,
+		V4,
+		V5,
+		V6,
+		V7,
+		V8,
+		V9,
+		V10,
+		J,
+		Q,
+		K
+	}
+	public static String[] valueStrings = {
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"J",
+		"Q",
+		"K"
+	};
+	public static String[] typeStrings = {
+		"Harts",
+		"Diamonds",
+		"Spades",
+		"Cloves"
+	};
+	public static String[] colorStrings = {
+		"red",
+		"black"
+	};
+	private int type;
+	private String color;
+	private int value;
+	private String rep;
+
+	public Card(int type, int value) {
+		this.type = type;
+		this.value = value;
+		update();
+	}
+	private void update(){
+		this.rep = typeStrings[type]+"-"+valueStrings[value];
+		this.color = (type %2 == 0 )? colorStrings[0] : colorStrings[1];
+		this.id = (type * 13 + value);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+		update();
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+		update();
+	}
+
+	@Override
+	public String toString() {
+		return this.rep; 
+	}
+	
+	
+}
---
+++ src/finaljava/Deck.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finaljava;
+
+import java.util.Random;
+import java.util.Stack;
+
+/**
+ *
+ * @author J. Garcia, jyo.garcia at gmail.com
+ */
+public class Deck {
+	public Stack<Card> cards;
+
+	public void fill(){
+		cards = new Stack<>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				cards.add(new Card(i,j));
+			}
+		}
+		System.out.println(cards.size());
+	}
+	public void scramble(){
+		Random rand = new Random();
+		Stack<Card> temp = new Stack<>();
+		for (int i = cards.size() -1 ; i > -1 ; i--){
+			int index = rand.nextInt(i+1);
+			temp.add(cards.remove(index));
+		}
+		cards = temp;
+	}
+	public void printDeck(){
+		for (int i = 0; i < cards.size(); i++) {
+			System.out.println(String.format("%d %s", i+1 ,  cards.get(i)));
+		}
+	}
+	public void sort(){
+		cards.sort((t, t1) -> {
+			Integer a = t.getId();
+			Integer b = t1.getId();
+			return a.compareTo(b); //To change body of generated lambdas, choose Tools | Templates.
+		});
+	
+	}
+	
+	public void flip(){
+		Stack<Card> temp = new Stack<>();
+		for (int i = cards.size() - 1 ; i > -1 ; i--){
+			temp.add(cards.remove(i));
+		}
+		cards = temp;
+	}
+
+	public Card remove(Card c){
+		Card found = null;
+		for (int i = 0; i < cards.size(); i++) {
+			if (c.toString().compareTo(cards.get(i).toString()) == 0 ) {
+				found = cards.remove(i);
+			}
+		}
+		return found;
+	}
+
+	public static void main(String[] args) {
+		// Deck instance
+		Deck d = new Deck();
+		// Fill deck
+		System.out.println("********************************************************************************");
+		System.out.println("Filling deck:"); 
+		System.out.println("********************************************************************************");
+		d.fill();
+		d.printDeck();
+
+		// Flip order
+		System.out.println("********************************************************************************");
+		System.out.println("Flipping deck:"); 
+		System.out.println("********************************************************************************");
+		d.flip();
+		d.printDeck();
+
+		// Removing cards
+		Card temp = null;
+		System.out.println("********************************************************************************");
+		System.out.println("Taking out cards:"); 
+		System.out.println("********************************************************************************");
+		temp = d.remove(new Card(Card.type.SPADES.ordinal(), Card.value.V10.ordinal()));
+		System.out.println(String.format("Removed: %s", 
+				(temp != null) ? temp.toString() : "none"));
+		temp = d.remove(new Card(Card.type.DIAMONDS.ordinal(), Card.value.K.ordinal()));
+		System.out.println(String.format("Removed: %s", 
+				(temp != null) ? temp.toString() : "none"));
+		temp = d.remove(new Card(Card.type.DIAMONDS.ordinal(), Card.value.V3.ordinal()));
+		System.out.println(String.format("Removed: %s", 
+				(temp != null) ? temp.toString() : "none"));
+		d.scramble();
+		System.out.println("********************************************************************************");
+		System.out.println("Cards left: " + d.cards.size());
+		System.out.println("********************************************************************************");
+		d.printDeck();
+
+		// Sorting
+		System.out.println("********************************************************************************");
+		System.out.println("Scrambled:");
+		System.out.println("********************************************************************************");
+		d.scramble();
+		d.printDeck();
+		System.out.println("********************************************************************************");
+		System.out.println("Ordered:");
+		System.out.println("********************************************************************************");
+		d.sort();
+		d.printDeck();
+	}
+	
+}
